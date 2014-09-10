23c595a6f6eaa9893bd88de416f7d11f84586860
xanxys <xanxys@gmail.com>
2014-09-10 22:22:46 +0900

    Stop generation of debug images by default. (30% faster)

--- preprocess.py
+++ preprocess.py
@@ -414,7 +414,7 @@ def extract_patches(ortho_image, xs, ys, margin=0.1, patch_size=80):
     return patches
 
 
-def detect_board(photo_id, img, visualize, derive):
+def detect_board(photo_id, img, visualize=False, derive=False):
     """
     * photo_id: str
     * img: BGR image
@@ -584,12 +584,12 @@ def derive_empty_vs_nonempty_samples(photo_id, patches):
 
 
 def process_image(packed_args):
-    photo_id, img_path, derive = packed_args
+    photo_id, img_path, debug, derive = packed_args
     print(img_path)
     try:
         img = cv2.imread(img_path)
         print('processing %s: id=%s shape=%s' % (img_path, photo_id, img.shape))
-        detected = detect_board(str(photo_id), img, True, derive)
+        detected = detect_board(str(photo_id), img, visualize=debug, derive=derive)
         if not detected:
             return {
                 "loaded": 1
@@ -621,6 +621,9 @@ Extract 9x9 cells from photos of shogi board.""",
     parser.add_argument(
         '--derive', action='store_true',
         help='Derive training data')
+    parser.add_argument(
+        '--debug', action='store_true',
+        help='Dump debug images to ./debug/')
 
     args = parser.parse_args()
     assert(args.j >= 1)
@@ -629,7 +632,7 @@ Extract 9x9 cells from photos of shogi board.""",
     pool = multiprocessing.Pool(args.j)
     # HACK: receive keyboard interrupt correctly
     # https://stackoverflow.com/questions/1408356/keyboard-interrupts-with-pythons-multiprocessing-pool
-    ls = [(os.path.splitext(p)[0], os.path.join(dir_path, p), args.derive) for p in os.listdir(dir_path)]
+    ls = [(os.path.splitext(p)[0], os.path.join(dir_path, p), args.debug, args.derive) for p in os.listdir(dir_path)]
     results = pool.map_async(process_image, ls).get(1000)
 
     count = {}
