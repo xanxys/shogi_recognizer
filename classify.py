08f378ef84fa97f2224e1de37a3fcb1eb6f5a142
xanxys <xanxys@gmail.com>
2014-09-11 17:07:30 +0900

    Split NN components and actual, tuned classifiers.

--- classify.py
+++ classify.py
@@ -13,6 +13,7 @@ import time
 import json
 import ujson
 import bz2
+import neuralnet
 
 
 def np_to_py(arr):
@@ -31,15 +32,10 @@ class CellEmptinessClassifier(object):
         x = T.matrix('x')
         self.x = x
 
-        # self.regression = LogisticRegression(input=x, n_in=400, n_out=2)
-        # self.cost = self.regression.negative_log_likelihood
-        # self.errors = self.regression.errors
-        # self.params = self.regression.params
-
         rng = np.random.RandomState(1234)
-        self.mlp = MLP(input=x, n_in=400, rng=rng, n_hidden=10, n_out=2)
+        self.mlp = neuralnet.MLP(
+            input=x, n_in=400, rng=rng, n_hidden=10, n_out=2)
 
-        # y = T.ivector('y')
         def gen_cost(y):
             return (
                 self.mlp.negative_log_likelihood(y) +
@@ -61,7 +57,7 @@ class CellEmptinessClassifier(object):
             ser_params.append(np_to_py(param.eval()))
 
         blob = {
-            "comment": "logistic regression, 20x20 image",
+            "comment": "MLP, 20x20 image",
             "mlp": ser_params
         }
         with bz2.BZ2File(path, 'w') as bz2_stream:
@@ -100,7 +96,8 @@ class CellTypeClassifierUp(object):
         x = T.matrix('x')
         self.x = x
 
-        self.regression = LogisticRegression(input=x, n_in=400, n_out=len(self.labels))
+        self.regression = neuralnet.LogisticRegression(
+            input=x, n_in=400, n_out=len(self.labels))
         self.classify_model = theano.function(
             inputs=[x],
             outputs=[self.regression.y_pred, self.regression.p_y_given_x])
@@ -109,11 +106,28 @@ class CellTypeClassifierUp(object):
         self.errors = self.regression.errors
         self.params = self.regression.params
 
-    def load_parameters(self, path):
-        self.regression.load_parameters(path)
-
     def dump_parameters(self, path):
-        self.regression.dump_parameters(path)
+        ser_params = []
+        for param in self.mlp.params:
+            ser_params.append(np_to_py(param.eval()))
+
+        blob = {
+            "comment": "logistic regression, 20x20 image",
+            "regression": ser_params
+        }
+        with bz2.BZ2File(path, 'w') as bz2_stream:
+            json.dump(blob, bz2_stream, separators=(',', ':'))
+
+    def load_parameters(self, path):
+        blob = json.load(bz2.BZ2File(path, 'r'))
+        if "b" in blob and "W" in blob:
+            # old format
+            self.regression.b.set_value(np.array(blob["b"]))
+            self.regression.W.set_value(np.array(blob["W"]))
+        else:
+            # new format
+            for (p, val) in zip(self.regression.params, blob["regression"]):
+                p.set_value(np.array(val))
 
     def get_label_to_category(self):
         return {label: cat for (cat, label) in enumerate(self.labels)}
@@ -129,262 +143,6 @@ class CellTypeClassifierUp(object):
         categories, probs = self.classify_model(vec.reshape([1, 400]))
         category = categories[0]
         return (self.get_category_to_label()[category], probs[0][category])
-
-
-class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
-                 activation=T.tanh):
-        """
-        Typical hidden layer of a MLP: units are fully-connected and have
-        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
-        and the bias vector b is of shape (n_out,).
-
-        NOTE : The nonlinearity used here is tanh
-
-        Hidden unit activation is given by: tanh(dot(input,W) + b)
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dmatrix
-        :param input: a symbolic tensor of shape (n_examples, n_in)
-
-        :type n_in: int
-        :param n_in: dimensionality of input
-
-        :type n_out: int
-        :param n_out: number of hidden units
-
-        :type activation: theano.Op or function
-        :param activation: Non linearity to be applied in the hidden
-                           layer
-        """
-        self.input = input
-
-        # `W` is initialized with `W_values` which is uniformely sampled
-        # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
-        # for tanh activation function
-        # the output of uniform if converted using asarray to dtype
-        # theano.config.floatX so that the code is runable on GPU
-        # Note : optimal initialization of weights is dependent on the
-        #        activation function used (among other things).
-        #        For example, results presented in [Xavier10] suggest that you
-        #        should use 4 times larger initial weights for sigmoid
-        #        compared to tanh
-        #        We have no info for other function, so we use the same as
-        #        tanh.
-        if W is None:
-            W_values = np.asarray(rng.uniform(
-                low=-np.sqrt(6. / (n_in + n_out)),
-                high=np.sqrt(6. / (n_in + n_out)),
-                size=(n_in, n_out)), dtype=theano.config.floatX)
-            if activation == theano.tensor.nnet.sigmoid:
-                W_values *= 4
-
-            W = theano.shared(value=W_values, name='W', borrow=True)
-
-        if b is None:
-            b_values = np.zeros((n_out,), dtype=theano.config.floatX)
-            b = theano.shared(value=b_values, name='b', borrow=True)
-
-        self.W = W
-        self.b = b
-
-        lin_output = T.dot(input, self.W) + self.b
-        self.output = (lin_output if activation is None
-                       else activation(lin_output))
-        # parameters of the model
-        self.params = [self.W, self.b]
-
-
-class MLP(object):
-    """Multi-Layer Perceptron Class
-
-    A multilayer perceptron is a feedforward artificial neural network model
-    that has one layer or more of hidden units and nonlinear activations.
-    Intermediate layers usually have as activation function tanh or the
-    sigmoid function (defined here by a ``HiddenLayer`` class)  while the
-    top layer is a softamx layer (defined here by a ``LogisticRegression``
-    class).
-    """
-
-    def __init__(self, rng, input, n_in, n_hidden, n_out):
-        """Initialize the parameters for the multilayer perceptron
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.TensorType
-        :param input: symbolic variable that describes the input of the
-        architecture (one minibatch)
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-        which the datapoints lie
-
-        :type n_hidden: int
-        :param n_hidden: number of hidden units
-
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-        which the labels lie
-
-        """
-
-        # Since we are dealing with a one hidden layer MLP, this will translate
-        # into a HiddenLayer with a tanh activation function connected to the
-        # LogisticRegression layer; the activation function can be replaced by
-        # sigmoid or any other nonlinear function
-        self.hiddenLayer = HiddenLayer(rng=rng, input=input,
-                                       n_in=n_in, n_out=n_hidden,
-                                       activation=T.tanh)
-
-        # The logistic regression layer gets as input the hidden units
-        # of the hidden layer
-        self.logRegressionLayer = LogisticRegression(
-            input=self.hiddenLayer.output,
-            n_in=n_hidden,
-            n_out=n_out)
-
-        # L1 norm ; one regularization option is to enforce L1 norm to
-        # be small
-        self.L1 = abs(self.hiddenLayer.W).sum() + abs(self.logRegressionLayer.W).sum()
-
-        # square of L2 norm ; one regularization option is to enforce
-        # square of L2 norm to be small
-        self.L2_sqr = (self.hiddenLayer.W ** 2).sum() + (self.logRegressionLayer.W ** 2).sum()
-
-        # negative log likelihood of the MLP is given by the negative
-        # log likelihood of the output of the model, computed in the
-        # logistic regression layer
-        self.negative_log_likelihood = self.logRegressionLayer.negative_log_likelihood
-        # same holds for the function computing the number of errors
-        self.errors = self.logRegressionLayer.errors
-
-        # the parameters of the model are the parameters of the two layer it is
-        # made out of
-        self.params = self.hiddenLayer.params + self.logRegressionLayer.params
-
-
-class LogisticRegression(object):
-    """Multi-class Logistic Regression Class
-
-    The logistic regression is fully described by a weight matrix :math:`W`
-    and bias vector :math:`b`. Classification is done by projecting data
-    points onto a set of hyperplanes, the distance to which is used to
-    determine a class membership probability.
-    """
-
-    def __init__(self, input, n_in, n_out):
-        """ Initialize the parameters of the logistic regression
-
-        :type input: theano.tensor.TensorType
-        :param input: symbolic variable that describes the input of the
-                      architecture (one minibatch)
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-                     which the datapoints lie
-
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-                      which the labels lie
-
-        """
-
-        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
-        self.W = theano.shared(value=np.zeros((n_in, n_out),
-                                                 dtype=theano.config.floatX),
-                                name='W', borrow=True)
-        # initialize the baises b as a vector of n_out 0s
-        self.b = theano.shared(value=np.zeros((n_out,),
-                                                 dtype=theano.config.floatX),
-                               name='b', borrow=True)
-
-        # compute vector of class-membership probabilities in symbolic form
-        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
-
-        # compute prediction as class whose probability is maximal in
-        # symbolic form
-        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
-
-        # parameters of the model
-        self.params = [self.W, self.b]
-
-    def dump_parameters(self, path):
-        """
-        Dump model parameters to path in .json.bz2 format.
-        Although it's somewhat human readable, format
-        will not be stable.
-        """
-        params_w = self.W.eval()
-        params_b = self.b.eval()
-        blob = {
-            "comment": "logistic regression, 20x20 image",
-            "W": map(list, params_w),
-            "b": list(params_b)
-        }
-        with bz2.BZ2File(path, 'w') as bz2_stream:
-            ujson.dump(blob, bz2_stream, separators=(',', ':'))
-
-    def load_parameters(self, path):
-        """
-        Load model parameters written by dump_parameters.
-        """
-        blob = ujson.load(bz2.BZ2File(path, 'r'))
-        self.W.set_value(np.array(blob["W"]))
-        self.b.set_value(np.array(blob["b"]))
-
-    def negative_log_likelihood(self, y):
-        """Return the mean of the negative log-likelihood of the prediction
-        of this model under a given target distribution.
-
-        .. math::
-
-            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
-            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|} \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
-                \ell (\theta=\{W,b\}, \mathcal{D})
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-
-        Note: we use the mean instead of the sum so that
-              the learning rate is less dependent on the batch size
-        """
-        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
-        # number of examples (call it n) in the minibatch
-        # T.arange(y.shape[0]) is a symbolic vector which will contain
-        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
-        # Log-Probabilities (call it LP) with one row per example and
-        # one column per class LP[T.arange(y.shape[0]),y] is a vector
-        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
-        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
-        # the mean (across minibatch examples) of the elements in v,
-        # i.e., the mean log-likelihood across the minibatch.
-        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
-
-    def errors(self, y):
-        """Return a float representing the number of errors in the minibatch
-        over the total number of examples of the minibatch ; zero one
-        loss over the size of the minibatch
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-        """
-
-        # check if y has same dimension of y_pred
-        if y.ndim != self.y_pred.ndim:
-            raise TypeError('y should have the same shape as self.y_pred',
-                ('y', target.type, 'y_pred', self.y_pred.type))
-        # check if y is of the correct datatype
-        if y.dtype.startswith('int'):
-            # the T.neq operator returns a vector of 0s and 1s, where 1
-            # represents a mistake in prediction
-            return T.mean(T.neq(self.y_pred, y))
-        else:
-            raise NotImplementedError()
 
 
 def preprocess_cell_image(img):
@@ -555,7 +313,8 @@ def load_up_dataset(classifier):
     ]
 
 
-def train_sgd(datasets, model, learning_rate=0.13, n_epochs=1000, batch_size=100):
+def train_sgd(
+        datasets, model, learning_rate=0.13, n_epochs=1000, batch_size=100):
     """
     Use stochastic gradient descent to train model.
     model must expose following theano function/variables:
@@ -622,11 +381,7 @@ def train_sgd(datasets, model, learning_rate=0.13, n_epochs=1000, batch_size=100
             x: train_set_x[index * batch_size:(index + 1) * batch_size],
             y: train_set_y[index * batch_size:(index + 1) * batch_size]})
 
-
-    ###############
-    # TRAIN MODEL #
-    ###############
-    print('... training the model')
+    print('Training model')
     # early-stopping parameters
     patience = 5000  # look as this many examples regardless
     patience_increase = 2  # wait this much longer when a new best is found
@@ -638,7 +393,6 @@ def train_sgd(datasets, model, learning_rate=0.13, n_epochs=1000, batch_size=100
                                   # on the validation set; in this case we
                                   # check every epoch
 
-    best_params = None
     best_validation_loss = np.inf
     test_score = 0.
     start_time = time.clock()
@@ -649,8 +403,7 @@ def train_sgd(datasets, model, learning_rate=0.13, n_epochs=1000, batch_size=100
         epoch = epoch + 1
         for minibatch_index in xrange(n_train_batches):
 
-            minibatch_avg_cost = train_model(minibatch_index)
-            # iteration number
+            train_model(minibatch_index)
             iter = (epoch - 1) * n_train_batches + minibatch_index
 
             if (iter + 1) % validation_frequency == 0:
@@ -691,7 +444,6 @@ def train_sgd(datasets, model, learning_rate=0.13, n_epochs=1000, batch_size=100
     duration = end_time - start_time
     print('validation(best): %f %% | test: %f %%' % (best_validation_loss * 100, test_score * 100))
     print('%d epochs | %.1fsec | %fepoch/sec' % (epoch, duration, epoch / duration))
-
 
 
 def command_train_cell_classifier(dump_path):
