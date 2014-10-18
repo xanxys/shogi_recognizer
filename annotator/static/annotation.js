0eb69ec7cc3a8660191057171f692b4af5e6dbce
xanxys <xanxys@gmail.com>
2014-10-18 17:56:06 +0900

    Always scale editor size.

--- annotator/static/annotation.js
+++ annotator/static/annotation.js
@@ -1,21 +1,33 @@
 
 var Editor = function(i) {
+	var std_width = 600;
+
 	var e = $('#editor_main')[0];
-	e.width = i.width;
-	e.height = i.height;
 
 	// Basic, immutable settings.
-	this.ctx = e.getContext('2d');
-	this.width = i.width;
-	this.height = i.height;
+	this.img_width = i.width;
+	this.img_height = i.height;
+	this.editor_width = std_width;
+	this.editor_height = Math.floor(i.height * this.editor_width / i.width);
 	this.image = i;
 
 	// Variables that editor modifies.
 	this.model = [];
+
+	// Set UI size.
+	e.width = this.editor_width;
+	e.height = this.editor_height;
+	this.ctx = e.getContext('2d');
 };
 
 Editor.prototype.redraw = function() {
 	var ctx = this.ctx;
+	ctx.save();
+	ctx.scale(
+		this.editor_width / this.img_width,
+		this.editor_height / this.img_height);
+	// Operate in image-space hereafter.
+
 	ctx.drawImage(this.image, 0, 0);
 
 	// update info
@@ -23,13 +35,21 @@ Editor.prototype.redraw = function() {
 
 	// draw grid region
 	ctx.beginPath();
-	ctx.strokeStyle = 'rgba(255, 0, 0, 0.8)';
-	ctx.lineWidth = 2;
-	_.each(this.model["corners"], function(corner, i) {
+	ctx.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+	ctx.lineWidth = 3;
+	_.each(this.model["corners"], function(corner) {
 		ctx.lineTo(corner[0], corner[1]);
 	});
 	ctx.closePath();
 	ctx.stroke();
+	if(this.model["corners"].length > 0) {
+		var pt = this.model["corners"][0];
+		ctx.beginPath();
+		ctx.arc(pt[0], pt[1], 5, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+	ctx.restore();
 };
 
 
