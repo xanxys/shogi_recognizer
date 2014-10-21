b10a58c22509587a697ddc58bd24fb045c3e3aec
xanxys <xanxys@gmail.com>
2014-10-21 12:06:13 +0900

    Refactor.

--- preprocess.py
+++ preprocess.py
@@ -17,6 +17,7 @@ import cairo
 import sqlite3
 import lsd
 import json
+import shogi
 
 
 def clean_directory(dir_path):
@@ -27,43 +28,6 @@ def clean_directory(dir_path):
         os.unlink(os.path.join(dir_path, path))
 
 
-def get_initial_configuration():
-    """
-    Return (pos, type)
-    pos: (1, 1) - (9, 9)
-    type will be 2-letter strings like CSA format.
-    (e.g. "FU", "HI", etc.)
-    """
-    initial_state_top = {
-        (1, 1): "KY",
-        (2, 1): "KE",
-        (3, 1): "GI",
-        (4, 1): "KI",
-        (5, 1): "OU",
-        (6, 1): "KI",
-        (7, 1): "GI",
-        (8, 1): "KE",
-        (9, 1): "KY",
-        (2, 2): "KA",
-        (8, 2): "HI",
-        (1, 3): "FU",
-        (2, 3): "FU",
-        (3, 3): "FU",
-        (4, 3): "FU",
-        (5, 3): "FU",
-        (6, 3): "FU",
-        (7, 3): "FU",
-        (8, 3): "FU",
-        (9, 3): "FU",
-    }
-    initial_state = {}
-    for (pos, ty) in initial_state_top.items():
-        x, y = pos
-        initial_state[pos] = ty
-        initial_state[(10 - x, 10 - y)] = ty
-    return initial_state
-
-
 def get_rot_invariants_initial():
     """
     Return positions invariant to 90-degree rotation,
@@ -71,7 +35,7 @@ def get_rot_invariants_initial():
 
     (always_empty, always_occupied)
     """
-    initial_state = get_initial_configuration()
+    initial_state = shogi.get_initial_configuration()
     common_occupied = set()
     common_empty = set()
     for i in range(1, 10):
@@ -776,7 +740,7 @@ def is_vertical(patches):
     param_path = "params/cell_emptiness_20x20.json.bz2"
 
     # Guess rotation by using empty vs. occupied information.
-    initial_state = get_initial_configuration()
+    initial_state = shogi.get_initial_configuration()
     always_empty, always_occupied = get_rot_invariants_initial()
     classifier_e = classify.CellEmptinessClassifier()
     classifier_e.load_parameters(param_path)
@@ -831,7 +795,7 @@ def derive_typed_samples(photo_id, patches):
     if not vertical:
         patches = rotate_patches_90deg(patches)
 
-    initial_conf = get_initial_configuration()
+    initial_conf = shogi.get_initial_configuration()
     for (pos, patch) in patches.items():
         img = patch["image"]
         label = "empty"
